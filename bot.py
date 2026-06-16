6371ade252d3f7d609923da8ba2b130672f4e082
tmtequipeacl-png <tmt.equipeacl@gmail.com>
2026-06-16 09:51:06 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -45,7 +45,7 @@ def job():
     
     enviar_mensagem_whatsapp(DESTINATARIO_UNICO, msg_teste)
 
-schedule.every().tuesday.at("09:55").do(job)
+schedule.every().tuesday.at("12:53").do(job)
 
 print("=" * 60)
 print("Robô de agendamento iniciado.")
